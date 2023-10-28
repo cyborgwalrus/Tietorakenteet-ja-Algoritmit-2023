4beb49d1a823551548f017074ccdb3c242032d60
Matias Björklund <32132417+cyborgwalrus@users.noreply.github.com>
2023-10-28 12:51:36 +0300

    Added LinkedListQueue to some factories

--- src/main/java/oy/interact/tira/factories/QueueFactory.java
+++ src/main/java/oy/interact/tira/factories/QueueFactory.java
@@ -1,7 +1,8 @@
 package oy.interact.tira.factories;
 
 import oy.interact.tira.model.Coder;
-import oy.interact.tira.student.QueueImplementation;
+import oy.interact.tira.student.ArrayQueue;
+import oy.interact.tira.student.LinkedListQueue;
 import oy.interact.tira.util.QueueInterface;
 
 public class QueueFactory {
@@ -11,22 +12,22 @@ public class QueueFactory {
 	}
 
 	public static QueueInterface<Integer> createIntegerQueue() {
-		return new QueueImplementation<Integer>();
+		return new LinkedListQueue<Integer>();
 	}
 
 	public static QueueInterface<Integer> createIntegerQueue(int capacity) {
-		return new QueueImplementation<Integer>(capacity);
+		return new ArrayQueue<Integer>(capacity);
 	}
 
 	public static QueueInterface<String> createStringQueue() {
-		return new QueueImplementation<String>();
+		return new LinkedListQueue<String>();
 	}
 
 	public static QueueInterface<String> createStringQueue(int capacity) {
-		return new QueueImplementation<String>(capacity);
+		return new ArrayQueue<String>(capacity);
 	}
 
 	public static QueueInterface<Coder> createCoderQueue() {
-		return new QueueImplementation<Coder>();
+		return new LinkedListQueue<Coder>();
 	}
 }
