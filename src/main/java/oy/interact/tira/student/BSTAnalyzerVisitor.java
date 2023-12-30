f74735feb61e79fa3f2a329b07710430e7b1e408
Matias Björklund <32132417+cyborgwalrus@users.noreply.github.com>
2023-12-30 17:23:49 +0200

    BSTAnalyzerVisitor tehty mutta ei testattu

--- src/main/java/oy/interact/tira/student/BSTAnalyzerVisitor.java
+++ src/main/java/oy/interact/tira/student/BSTAnalyzerVisitor.java
@@ -8,36 +8,56 @@ public class BSTAnalyzerVisitor<K extends Comparable<K>, V> implements Visitor<K
     private int numberOfLeafNodes;
     private int minimumDepth;
     private int maximumDepth;
+    private int sumOfDepths;
     private int averageDepth;
     private int idealDepth;
+    private int currentDepth;
 
     public BSTAnalyzerVisitor() {
         this.numberOfElements = 0;
         this.numberOfLeafNodes = 0;
         this.minimumDepth = 0;
         this.maximumDepth = 0;
+        this.sumOfDepths = 0;
         this.averageDepth = 0;
         this.idealDepth = 0;
     }
 
     @Override
     public void visit(Visitable<K, V> visitable) throws Exception {
-        if(visitable != null){
+        if (visitable != null) {
             visitable.accept(this);
         }
     }
 
     public void visit(BinarySearchTreeContainer<K, V> binarySearchTree) throws Exception {
         if (binarySearchTree != null && binarySearchTree.root != null) {
-            this.numberOfElements = binarySearchTree.root.numberOfChildren + 1;
+            numberOfElements = binarySearchTree.root.numberOfChildren + 1;
             binarySearchTree.root.accept(this);
+            // BST visit complete, processing remaining variables
+            averageDepth = sumOfDepths / numberOfElements;
+            idealDepth = (int)Math.log(numberOfElements);
         }
 
     }
 
     public void visit(TreeNode<K, V> node) throws Exception {
-        if(node != null){
-            
+        if (node != null) {
+            currentDepth++;
+            // Check for leaf nodes
+            if (node.left == null && node.right == null)
+                numberOfLeafNodes++;
+            // visit child nodes
+            node.left.accept(this);
+            node.right.accept(this);
+
+        } else {
+            // node == null, meaning we reached the end of a branch
+            sumOfDepths += currentDepth;
+            if (currentDepth < minimumDepth)
+                minimumDepth = currentDepth;
+            if (currentDepth > maximumDepth)
+                maximumDepth = currentDepth;
         }
     }
 
@@ -49,8 +69,16 @@ public class BSTAnalyzerVisitor<K extends Comparable<K>, V> implements Visitor<K
 
     @Override
     public String toString() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'toString'");
+        String string = "Tree analytics:\n";
+        string += String.format("- Elements: %d\n", numberOfElements);
+        string += String.format("- Leaf nodes: %d\n", numberOfLeafNodes);
+        string += String.format("- min depth: %d\n", minimumDepth);
+        string += String.format("- max depth: %d\n", maximumDepth);
+        string += String.format("- average depth: %d\n", averageDepth);
+        string += String.format("- average depth: %d\n", averageDepth);
+        string += String.format("- Ideal depth for balanced BST would be: %d\n", idealDepth);
+
+        return string;
     }
 
 }
